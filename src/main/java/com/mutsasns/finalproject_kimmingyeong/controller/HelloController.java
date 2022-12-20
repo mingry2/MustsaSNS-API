6d75084cb73cdc1e116367b8d0a56c0f7fb23950
minkyoung kim <minry20415@naver.com>
2022-12-20 13:42:55 +0900

    1. HelloController -> just hello 2. .properties -> .yml 3. 디비 환경변수 설정 4. SwaggerConfiguration 추가

---
+++ src/main/java/com/mutsasns/finalproject_kimmingyeong/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.mutsasns.finalproject_kimmingyeong.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    // http://localhost:8080/api/v1/hello
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
+    }
+}
