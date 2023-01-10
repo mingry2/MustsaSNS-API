67bbca6cc4cff14f11467f7237afea37458fe465
minkyoung kim <minry20415@naver.com>
2023-01-10 17:39:14 +0900

    Swagger 수정

--- src/main/java/com/mutsasns/finalproject_kimmingyeong/controller/LikeRestController.java
+++ src/main/java/com/mutsasns/finalproject_kimmingyeong/controller/LikeRestController.java
@@ -3,12 +3,14 @@ package com.mutsasns.finalproject_kimmingyeong.controller;
 import com.mutsasns.finalproject_kimmingyeong.domain.dto.like.LikeAddResponse;
 import com.mutsasns.finalproject_kimmingyeong.domain.dto.response.Response;
 import com.mutsasns.finalproject_kimmingyeong.service.LikeService;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -23,8 +25,15 @@ public class LikeRestController {
     @ApiOperation(
             value = "좋아요 누르기"
             , notes = "like 한 번 누르면 1, 한 번 더 누르면 0(soft deletedAt 적용) -> 중복 불가")
+    @ApiImplicitParam(
+            name = "postId"
+            , value = "포스트 ID"
+            , required = true
+            , dataType = "long"
+            , paramType = "path"
+            , defaultValue = "None")
     @PostMapping("/{postId}/likes")
-    public Response<LikeAddResponse> pushLike(@PathVariable Long postId, Authentication authentication){
+    public Response<LikeAddResponse> pushLike(@PathVariable Long postId, @ApiIgnore Authentication authentication){
         likeService.addLike(postId, authentication.getName());
         return Response.success(LikeAddResponse.builder()
                                             .message("좋아요를 눌렀습니다.")
@@ -36,8 +45,15 @@ public class LikeRestController {
     @ApiOperation(
             value = "좋아요 조회"
             , notes = "포스트 좋아요 개수 조회")
+    @ApiImplicitParam(
+            name = "postId"
+            , value = "포스트 ID"
+            , required = true
+            , dataType = "long"
+            , paramType = "path"
+            , defaultValue = "None")
     @GetMapping("/{postId}/likes")
-    public Response<Long> countLike(@PathVariable Long postId, Authentication authentication){
+    public Response<Long> countLike(@PathVariable Long postId, @ApiIgnore Authentication authentication){
         Long likeCount = likeService.countLike(postId, authentication.getName());
         return Response.success(likeCount);
 
--- src/main/java/com/mutsasns/finalproject_kimmingyeong/controller/PostRestController.java
+++ src/main/java/com/mutsasns/finalproject_kimmingyeong/controller/PostRestController.java
@@ -3,6 +3,7 @@ package com.mutsasns.finalproject_kimmingyeong.controller;
 import com.mutsasns.finalproject_kimmingyeong.domain.dto.post.*;
 import com.mutsasns.finalproject_kimmingyeong.domain.dto.response.Response;
 import com.mutsasns.finalproject_kimmingyeong.service.PostService;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
 import static com.sun.tools.attach.VirtualMachine.list;
 
 @RestController
@@ -30,7 +33,7 @@ public class PostRestController {
             value = "포스트 작성"
             , notes = "title, body -> 포스트 작성 - 회원만 작성 가능(token 필)")
     @PostMapping("")
-    public Response<PostResponse> createPost(@RequestBody PostRequest postRequest, Authentication authentication){
+    public Response<PostResponse> createPost(@RequestBody PostRequest postRequest, @ApiIgnore Authentication authentication){
         log.info("title : {} body : {}", postRequest.getTitle(), postRequest.getBody());
         PostResponse postResponse = postService.create(postRequest.getTitle(), postRequest.getBody(), authentication.getName());
         return Response.success(postResponse);
@@ -41,7 +44,7 @@ public class PostRestController {
             value = "포스트 조회(전체)"
             , notes = "모든 포스트 조회 - 회원/비회원 모두 조회 가능")
     @GetMapping("")
-    public Response<Page<PostListResponse>> postList(@PageableDefault(size = 20) @SortDefault(sort = "createdAt",direction = Sort.Direction.DESC) Pageable pageable){
+    public Response<Page<PostListResponse>> postList(@ApiIgnore @PageableDefault(size = 20) @SortDefault(sort = "createdAt",direction = Sort.Direction.DESC) Pageable pageable){
         Page<PostListResponse> list = postService.getAll(pageable);
         log.info("list : {} ", list());
         return Response.success(list);
@@ -51,6 +54,13 @@ public class PostRestController {
     @ApiOperation(
             value = "포스트 조회(상세)"
             , notes = "포스트 1개 조회 - 회원/비회원 모두 조회 가능")
+    @ApiImplicitParam(
+            name = "postId"
+            , value = "포스트 ID"
+            , required = true
+            , dataType = "long"
+            , paramType = "path"
+            , defaultValue = "None")
     @GetMapping("/{postId}")
     public Response<PostListResponse> findById(@PathVariable Long postId){
         log.debug("postId : {} ", postId);
@@ -63,8 +73,15 @@ public class PostRestController {
     @ApiOperation(
             value = "포스트 수정"
             , notes = "title, body -> 포스트 수정 - 포스트 등록한 회원만 수정 가능(token 필)")
+    @ApiImplicitParam(
+            name = "postId"
+            , value = "포스트 ID"
+            , required = true
+            , dataType = "long"
+            , paramType = "path"
+            , defaultValue = "None")
     @PutMapping("/{postId}")
-    public Response<PostResponse> modify(@PathVariable Long postId, @RequestBody PostRequest postModifyRequest, Authentication authentication){
+    public Response<PostResponse> modify(@PathVariable Long postId, @RequestBody PostRequest postModifyRequest, @ApiIgnore Authentication authentication){
         log.debug("userName : {} postId : {} title : {} body : {} ", authentication.getName(), postId, postModifyRequest.getTitle(), postModifyRequest.getBody());
         PostResponse postModifiedResponse = postService.modify(authentication.getName(), postId, postModifyRequest.getTitle(), postModifyRequest.getBody());
         return Response.success(postModifiedResponse);
@@ -74,8 +91,15 @@ public class PostRestController {
     @ApiOperation(
             value = "포스트 삭제"
             , notes = "포스트 삭제 - 포스트 등록한 회원만 삭제 가능(token 필)")
+    @ApiImplicitParam(
+            name = "postId"
+            , value = "포스트 ID"
+            , required = true
+            , dataType = "long"
+            , paramType = "path"
+            , defaultValue = "None")
     @DeleteMapping("/{postId}")
-    public Response<PostResponse> delete(@PathVariable Long postId, Authentication authentication){
+    public Response<PostResponse> delete(@PathVariable Long postId, @ApiIgnore Authentication authentication){
         postService.delete(postId, authentication.getName());
         PostResponse postDeletedResponse = PostResponse.builder()
                 .message("포스트 삭제 완료")
@@ -86,7 +110,7 @@ public class PostRestController {
 
     // 마이피드 조회
     @GetMapping("/my")
-    public Response<Page<PostListResponse>> myFeed(Authentication authentication, @PageableDefault(size = 20) @SortDefault(sort = "createdAt",direction = Sort.Direction.DESC) Pageable pageable){
+    public Response<Page<PostListResponse>> myFeed(@ApiIgnore Authentication authentication, @ApiIgnore @PageableDefault(size = 20) @SortDefault(sort = "createdAt",direction = Sort.Direction.DESC) Pageable pageable){
         Page<PostListResponse> myFeed = postService.myFeedAll(authentication.getName(), pageable);
         return Response.success(myFeed);
 
